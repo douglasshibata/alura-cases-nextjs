d51dac132fd2863268ae628eefee4fbddf4d1f96
Douglas Shibata <douglas.shibata11@gmail.com>
2022-10-31 10:08:30 -0300

    Adicionando estilos, theme e começando o faq

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,12 @@
-
 import Link from '../src/components/Link';
-import styles from '../styles/Home.module.css';
 
 export default function Page404() {
-  return (
-    <div className={styles.container}>
-      <h1>Página não encontrada</h1>
-      <Link href='/'>
-        Voltar para a Home
-      </Link>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Você se perdeu e caiu na página 404 :O</h1>
+            <Link href="/">
+                Ir para a home
+            </Link>
+        </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import Link from '../src/components/Link';
-import Title from '../src/components/Title';
-import styles from '../styles/Home.module.css';
+import HomeScreen from '../src/screens/HomeScreen';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Title as="h1">Alura Cases - Home Page</Title>
-      <Link href='/faq'>
-        Faq
-      </Link>
-    </div>
-  );
-}
+export default HomeScreen;
+
+// import React from 'react';
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <React.Fragment>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </React.Fragment>
+//     );
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h2">Alura Cases - Home</Title>
+//             <Link href="/faq">
+//                 Ir para a página do FAQ
+//             </Link>
+//             <Footer />
+//         </div>
+//     )
+// }
